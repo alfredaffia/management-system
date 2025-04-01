3e5a41a6c78d32b377fbfb34269dcd351ff0ca39
alfredaffia <alfredaffia478@gmail.com>
2025-04-01 15:58:30 +0100

    resource generation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TodoModule } from './todo/todo.module';
+import { UserModule } from './user/user.module';
 
 
 @Module({
-  imports: [],
+  imports: [TodoModule, UserModule],
   controllers: [],
   providers: [],
 })
