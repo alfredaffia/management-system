a8efdfee9fb08ed2aa9a429fae6c782a204da12e
alfredaffia <alfredaffia478@gmail.com>
2025-04-01 16:09:12 +0100

    addition of entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,7 +1,20 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn('uuid') 
-  id: number;
+    @PrimaryGeneratedColumn('uuid')
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column({ unique: true })
+    email: string;
+    
+    @Column()
+    password: string;
+
 }
