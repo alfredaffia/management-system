76a94a49488cf103304509df5d92b537b6bbfee7
alfredaffia <alfredaffia478@gmail.com>
2025-04-01 17:00:54 +0100

    Database configuration 2

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+// import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserService {
+  constructor(@InjectRepository(User)
+  private createUserDto: Repository<User>,
+    // private jwtService: JwtService,
+  ) { }
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
