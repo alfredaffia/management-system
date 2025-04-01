69b583f206e3617a23cdb75e86b56c44c875c2d4
alfredaffia <alfredaffia478@gmail.com>
2025-04-01 16:49:00 +0100

    Database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
 import { UserModule } from './user/user.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [TodoModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),  
+    TypeOrmModule.forRootAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.getOrThrow('DB_HOST'),
+        port: configService.getOrThrow('DB_PORT'),
+        username: configService.getOrThrow('DB_USERNAME'),
+        password: configService.getOrThrow('DB_PASSWORD'),
+        database: configService.getOrThrow('DB_NAME'),
+        entities: [User], // Ensure this line includes the User entity
+        synchronize: true,
+      })
+      }),
+    TodoModule, UserModule],
   controllers: [],
   providers: [],
 })
